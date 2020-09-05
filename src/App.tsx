9cd1b9c01b0b8d6aa17666d03e6ebdbd9ce7afa7
Chisw <chisw@qq.com>
2020-09-05 22:25:22 +0800

    use element-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Button } from 'element-react'
+import blueprint from './images/blueprint.svg'
+import 'element-theme-default'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div className="bg-red-200 border">
-        a
+      <div
+        className="absolute inset-0 p-12 bg-center flex flex-col"
+        style={{ backgroundImage: `url(${blueprint}), linear-gradient(0, rgba(10, 37, 110), rgba(5, 20, 55))` }}
+      >
+        <div>
+          <h2 className="text-white text-3xl font-bold">水印大师</h2>
+        </div>
+        <div className="mt-8 flex flex-grow">
+          <div className="mr-8 w-64">
+            <Button type="primary" icon="plus" size="large">创建水印项</Button>
+          </div>
+          <div className="flex-grow border-2 border-dashed rounded-lg">
+
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Settings.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Settings() {
+  return (
+    <>
+      格式
+        原格式
+        .jpg
+          .92
+          .86
+          .72
+        .png
+        .webp
+      尺寸
+        固定长宽比
+        宽度
+          固定像素
+          百分比
+        高度
+    </>
+  )
+}
