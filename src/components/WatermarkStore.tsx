52730cc294c6cc5002fa3ce55f1d649955d059e9
Chisw <chisw@qq.com>
2020-09-17 19:33:16 +0800

    Create WatermarkStore.tsx

---
+++ src/components/WatermarkStore.tsx
@@ -0,0 +1,26 @@
+import React form 'react'
+import { IWatermark } from '../ts/type'
+
+export default function WatermarkStore() {
+
+  const [storeList, setStoreList] = useState<IWatermark[]>([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    window.fetch(`https://kacha.jisuowei.com/kacha.store.json`)
+      .then(res => res.json())
+      .then(data => {
+        setStoreList(data)
+        setLoading(false)
+      })
+  }, [])
+
+  return (
+    <>
+      <div className="relative">
+         {loading && <span>loading</span> }
+      </div>
+    </>
+  )
+}
